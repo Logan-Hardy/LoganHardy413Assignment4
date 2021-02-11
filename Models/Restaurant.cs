5e3e0c7a9d9745046aa1e020faef332636338a89
Logan-Hardy <70916374+Logan-Hardy@users.noreply.github.com>
2021-02-10 20:16:03 -0700

    Added Additional Comments

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -38,41 +38,41 @@ namespace LoganHardy413Assignment4.Models
             
             Restaurant r1 = new Restaurant(1)
             {
-                RestaurantName = "outback",
-                FavoriteDish = "ribs",
-                Address = "372 e university pkwy, orem, ut 84058",
+                RestaurantName = "Outback",
+                FavoriteDish = "Ribs",
+                Address = "372 E University Pkwy, Orem, UT 84058",
                 PhoneNumber = "801-764-0552",
                 //Website = "https://locations.outback.com/" //When webiste is left null, "Coming soon" is the default for the website in the model
             };
             Restaurant r2 = new Restaurant(2)
             {
-                RestaurantName = "in-n-out",
+                RestaurantName = "In-n-Out",
                 //FavoriteDish = "burger", //FavoriteDish is stored as null, later will be displayed as "It's all tasty!" through the controller
-                Address = "350 e university pkwy, orem, ut 84058",
+                Address = "350 E University Pkwy, Orem, UT 84058",
                 PhoneNumber = "800-786-1000",
                 Website = "https://locations.in-n-out.com/235"
             };
             Restaurant r3 = new Restaurant(3)
             {
-                RestaurantName = "cubby's",
-                FavoriteDish = "burger",
-                Address = "1258 n state st, provo, ut 84604",
+                RestaurantName = "Cubby's",
+                FavoriteDish = "Burger",
+                Address = "1258 N State St, Provo, UT 84604",
                 //PhoneNumber = "801-919-3023", //accepts null phone numbers and is handled in the controller
                 Website = "http://cubbys.co/"
             };
             Restaurant r4 = new Restaurant(4)
             {
-                RestaurantName = "culver's",
-                FavoriteDish = "burger",
-                Address = "943 n 700 e, spanish fork, ut 84660",
+                RestaurantName = "Culver's",
+                FavoriteDish = "Burger",
+                Address = "943 N 700 E, Spanish Fork, UT 84660",
                 PhoneNumber = "801-504-6249",
                 Website = "https://www.culvers.com/"
             };
             Restaurant r5 = new Restaurant(5)
             {
-                RestaurantName = "cravings bistro",
-                FavoriteDish = "turkey sandwich",
-                Address = "25 w center st, pleasant grove, ut 84062",
+                RestaurantName = "Cravings Bistro",
+                FavoriteDish = "Turkey sandwich",
+                Address = "25 W Center St, Pleasant Grove, UT 84062",
                 PhoneNumber = "801-785-2439",
                 Website = "https://cravingsbistro.com/"
             };
